9308154b3f9cf6d8bd6232103cc025a5b309c082
GOPI CH <ch.gopi7893@gmail.com>
2024-05-23 08:50:12 +0530

    Add files via upload

---
+++ Projects/UserManagementSystem/Usermanagementsystem/src/main/java/userManagementSystemBackend/dto/Employeedto.java
@@ -0,0 +1,22 @@
+package userManagementSystemBackend.dto;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Employeedto {
+        private Long id;
+
+        private String firstName;
+
+        private String lastName;
+
+        private String email;
+    }
+
---
+++ Projects/UserManagementSystem/Usermanagementsystem/src/main/java/userManagementSystemBackend/service/employeeServiceImpl/Employeeserviceimpl.java
@@ -0,0 +1,59 @@
+package userManagementSystemBackend.service.employeeServiceImpl;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+import userManagementSystemBackend.dto.Employeedto;
+import userManagementSystemBackend.employeeMapper.Employeemapper;
+import userManagementSystemBackend.entity.Employee;
+import userManagementSystemBackend.exception.Resourcenotfoundexception;
+import userManagementSystemBackend.repository.Employeerepository;
+import userManagementSystemBackend.service.Employeeservice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class Employeeserviceimpl implements Employeeservice {
+
+    private Employeerepository employeeRepository;
+    @Override
+    public Employeedto createEmployee(Employeedto employeeDto) {
+        Employee employee= Employeemapper.mapToEmployee(employeeDto);
+        Employee savedEmployee= employeeRepository.save(employee);
+        return Employeemapper.mapToEmployeedto(savedEmployee);
+
+    }
+
+    @Override
+    public Employeedto getEmployeeById(Long employeeId) {
+        Employee employee =employeeRepository.findById(employeeId).orElseThrow(()-> new Resourcenotfoundexception("employee doesn't exist with given id"+ employeeId));
+        return Employeemapper.mapToEmployeedto(employee);
+    }
+    public List<Employeedto> getAllEmployees(){
+        List<Employee> employees= employeeRepository.findAll();
+        return employees.stream().map((employee) -> (Employeemapper.mapToEmployeedto(employee))).collect(Collectors.toList());
+
+
+    }
+
+    @Override
+    public Employeedto updateEmployee(Long employeeId, Employeedto updatedEmployee) {
+        Employee employee =employeeRepository.findById(employeeId).orElseThrow(()-> new Resourcenotfoundexception("employee doesn't exist with given id"+ employeeId));
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+        Employee updatedEmployeeobj = employeeRepository.save(employee);
+
+        return Employeemapper.mapToEmployeedto(updatedEmployeeobj);
+
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee =employeeRepository.findById(employeeId).orElseThrow(()-> new Resourcenotfoundexception("employee doesn't exist with given id"+ employeeId));
+        employeeRepository.deleteById(employeeId);
+
+    }
+}
