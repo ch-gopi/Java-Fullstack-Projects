52da4f02c7df5bfad4bcd301fb2bfc5f2355b760
GOPI CH <ch.gopi7893@gmail.com>
2025-04-30 22:13:09 +0530

    Add files via upload

---
+++ Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/Bug-Tracking-Application/Bug-Tracking-Application/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/exception/ErrorDetails.java
@@ -0,0 +1,18 @@
+package Bug.Tracking.System.Application.Bug.Tracking.Application.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+    private LocalDateTime timeStamp;
+    private String message;
+    private String details;
+}
