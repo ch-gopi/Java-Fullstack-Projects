273ce95f77aca10d071a20324ae9cfb6f1c06159
GOPI CH <ch.gopi7893@gmail.com>
2025-05-03 19:18:45 +0530

    Delete Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/Bug-Tracking-Application/Bug-Tracking-Application directory

--- Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/Bug-Tracking-Application/Bug-Tracking-Application/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/repository/Bugrepository.java
+++
@@ -1,8 +0,0 @@
-package Bug.Tracking.System.Application.Bug.Tracking.Application.repository;
-
-
-import Bug.Tracking.System.Application.Bug.Tracking.Application.entity.Bug;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Bugrepository extends JpaRepository<Bug, Long> {
-}
