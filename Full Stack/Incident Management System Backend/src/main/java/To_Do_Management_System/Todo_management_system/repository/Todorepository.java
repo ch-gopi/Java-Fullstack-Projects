7e308e73ff927f3d9e789181f51e172f5cdd0fc0
GOPI CH <ch.gopi7893@gmail.com>
2025-05-05 21:11:47 +0530

    Delete Full Stack/Incident Management System Backend directory

--- Full Stack/Incident Management System Backend/src/main/java/To_Do_Management_System/Todo_management_system/repository/Todorepository.java
+++
@@ -1,7 +0,0 @@
-package To_Do_Management_System.Todo_management_system.repository;
-
-import To_Do_Management_System.Todo_management_system.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Todorepository extends JpaRepository<Todo, Long> {
-}
