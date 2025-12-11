0e60ef71af558cb2eeb7a2a276b5db25606b9647
GOPI CH <ch.gopi7893@gmail.com>
2025-12-11 08:36:43 +0530

    Update EmailServiceImpl.java

--- Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/service/bugServiceImpl/EmailServiceImpl.java
+++ Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/service/bugServiceImpl/EmailServiceImpl.java
@@ -1,28 +1,29 @@
-package Bug.Tracking.System.Application.Bug.Tracking.Application.service.bugServiceImpl;
+// package Bug.Tracking.System.Application.Bug.Tracking.Application.service.bugServiceImpl;
 
 
-import lombok.AllArgsConstructor;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.stereotype.Service;
+// import lombok.AllArgsConstructor;
+// import org.springframework.mail.javamail.JavaMailSender;
+// import org.springframework.mail.javamail.MimeMessageHelper;
+// import org.springframework.stereotype.Service;
 
 
-import jakarta.mail.internet.MimeMessage;
-import jakarta.mail.MessagingException;
+// import jakarta.mail.internet.MimeMessage;
+// import jakarta.mail.MessagingException;
 
-@Service
-@AllArgsConstructor
-public class EmailServiceImpl {
+// @Service
+// @AllArgsConstructor
+// public class EmailServiceImpl {
 
-    private final JavaMailSender mailSender;
+//     private final JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String body) throws MessagingException {
-        MimeMessage message = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-        helper.setTo(to);
-        helper.setSubject(subject);
-        helper.setText(body, true); // HTML format
+//     public void sendEmail(String to, String subject, String body) throws MessagingException {
+//         MimeMessage message = mailSender.createMimeMessage();
+//         MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//         helper.setTo(to);
+//         helper.setSubject(subject);
+//         helper.setText(body, true); // HTML format
 
-        mailSender.send(message);
-    }
-}
+//         mailSender.send(message);
+//     }
+// }
+
