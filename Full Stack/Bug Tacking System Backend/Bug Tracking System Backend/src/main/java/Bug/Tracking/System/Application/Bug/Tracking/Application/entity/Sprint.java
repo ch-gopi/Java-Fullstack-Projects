48ae362fb102aa0f89374b4bc2101822695bf7f8
GOPI CH <ch.gopi7893@gmail.com>
2026-05-03 14:56:40 +0530

    Refactor Sprint entity with column constraints
    Updated Sprint entity to include column constraints and override equals and hashCode methods.

--- Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/entity/Sprint.java
+++ Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/entity/Sprint.java
@@ -8,54 +8,39 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
-@Table(name = "Sprint")
 @Entity
+@Table(name = "sprints")
 public class Sprint {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String sprintName;
+
+    @Column(nullable = false)
     private LocalDate startDate;
+
+    @Column(nullable = false)
     private LocalDate endDate;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Sprint)) return false;
+        Sprint sprint = (Sprint) o;
+        return id != null && id.equals(sprint.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
-=======
-package Bug.Tracking.System.Application.Bug.Tracking.Application.entity;
-
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDate;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-
-@Table(name = "Sprint")
-@Entity
-public class Sprint {
-
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String sprintName;
-    private LocalDate startDate;
-    private LocalDate endDate;
-
-
-}
->>>>>>> 7597e7e0eba66a899deb947e73815869450259fd
