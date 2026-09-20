511c6d8d2fab603a9cffecf32c3c094ea968811a
GOPI CH <ch.gopi7893@gmail.com>
2026-09-20 10:48:30 +0530

    Remove duplicate BugCommentRepository code

--- Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/repository/BugCommentRepository.java
+++ Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/repository/BugCommentRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Bug.Tracking.System.Application.Bug.Tracking.Application.repository;
 
 import Bug.Tracking.System.Application.Bug.Tracking.Application.entity.BugComment;
@@ -13,19 +12,3 @@ public interface BugCommentRepository extends JpaRepository<BugComment, Long> {
 
     void deleteByBugId(Long id);
 }
-=======
-package Bug.Tracking.System.Application.Bug.Tracking.Application.repository;
-
-import Bug.Tracking.System.Application.Bug.Tracking.Application.entity.BugComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-public interface BugCommentRepository extends JpaRepository<BugComment, Long> {
-    List<BugComment> findByBugId(Long bugId);
-
-    void deleteByBugId(Long id);
-}
->>>>>>> 7597e7e0eba66a899deb947e73815869450259fd
