fb9143a28a124f6de4c8508478a571e06fe397cc
GOPI CH <ch.gopi7893@gmail.com>
2026-09-20 10:47:56 +0530

    Update JwtAuthenticationEntryPoint.java

--- Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/security/JwtAuthenticationEntryPoint.java
+++ Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/security/JwtAuthenticationEntryPoint.java
@@ -1,9 +1,6 @@
 package Bug.Tracking.System.Application.Bug.Tracking.Application.security;
 
-<<<<<<< HEAD
 import Bug.Tracking.System.Application.Bug.Tracking.Application.config.AuthenticationEntryPointStrategy;
-=======
->>>>>>> 7597e7e0eba66a899deb947e73815869450259fd
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,13 +11,9 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
-<<<<<<< HEAD
-public class JwtAuthenticationEntryPoint implements AuthenticationEntryPointStrategy {
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPointStrategy, AuthenticationEntryPoint {
 
-=======
-public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
->>>>>>> 7597e7e0eba66a899deb947e73815869450259fd
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
