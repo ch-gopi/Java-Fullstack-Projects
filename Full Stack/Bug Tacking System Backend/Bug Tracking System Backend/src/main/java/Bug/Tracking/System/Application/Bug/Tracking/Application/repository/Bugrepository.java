73b7fe3ba25268326753479c27780388bc5d3803
GOPI CH <ch.gopi7893@gmail.com>
2026-09-20 10:48:49 +0530

    Refactor Bugrepository interface methods

--- Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/repository/Bugrepository.java
+++ Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/repository/Bugrepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Bug.Tracking.System.Application.Bug.Tracking.Application.repository;
 
 
@@ -14,59 +13,20 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.List;
 public interface Bugrepository extends JpaRepository<Bug, Long>, JpaSpecificationExecutor<Bug> {
 
-    long countBySeverity(Severity critical); 
+    long countBySeverity(Severity critical);
 
-    long countByCompleted(boolean b); 
-   
+    long countByCompleted(boolean b);
+
 
     @EntityGraph(attributePaths = {"user", "sprint"})
     Page<Bug> findAll(Pageable pageable);
-   
+
 
     @EntityGraph(attributePaths = {"user", "sprint"})
     List<Bug> findBySprintId(Long sprintId);
-   
+
 
     @EntityGraph(attributePaths = {"user", "sprint"})
     List<Bug> findAll(Specification<Bug> spec);
-   
+
 }
-
-
-
-=======
-package Bug.Tracking.System.Application.Bug.Tracking.Application.repository;
-
-
-import Bug.Tracking.System.Application.Bug.Tracking.Application.Enums.Severity;
-import Bug.Tracking.System.Application.Bug.Tracking.Application.entity.Bug;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-import java.util.List;
-public interface Bugrepository extends JpaRepository<Bug, Long>, JpaSpecificationExecutor<Bug> {
-
-    long countBySeverity(Severity critical); 
-
-    long countByCompleted(boolean b); 
-   
-
-    @EntityGraph(attributePaths = {"user", "sprint"})
-    Page<Bug> findAll(Pageable pageable);
-   
-
-    @EntityGraph(attributePaths = {"user", "sprint"})
-    List<Bug> findBySprintId(Long sprintId);
-   
-
-    @EntityGraph(attributePaths = {"user", "sprint"})
-    List<Bug> findAll(Specification<Bug> spec);
-   
-}
-
-
-
->>>>>>> 7597e7e0eba66a899deb947e73815869450259fd
