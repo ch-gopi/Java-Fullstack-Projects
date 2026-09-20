5878deb2f2fd2945f0e9160c19d25eba4711c4b1
GOPI CH <ch.gopi7893@gmail.com>
2026-09-20 10:43:00 +0530

    Refactor Severity enum to remove duplication
    Removed duplicate package declaration and cleaned up the Severity enum.

--- Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/Enums/Severity.java
+++ Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/Enums/Severity.java
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 package Bug.Tracking.System.Application.Bug.Tracking.Application.Enums;
 
 
 public enum Severity {
     LOW, MEDIUM, HIGH, CRITICAL
 }
-=======
-package Bug.Tracking.System.Application.Bug.Tracking.Application.Enums;
-
-
-public enum Severity {
-    LOW, MEDIUM, HIGH, CRITICAL
-}
->>>>>>> 7597e7e0eba66a899deb947e73815869450259fd
