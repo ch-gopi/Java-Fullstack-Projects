ffb9ea78cde474f43443d92ab9ed2e40ba7d9a53
GOPI CH <ch.gopi7893@gmail.com>
2026-09-20 10:49:17 +0530

    Remove merge conflict markers from SprintRepository

--- Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/repository/SprintRepository.java
+++ Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/repository/SprintRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Bug.Tracking.System.Application.Bug.Tracking.Application.repository;
 
 
@@ -9,19 +8,3 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SprintRepository extends JpaRepository<Sprint, Long> {
 }
-
-
-=======
-package Bug.Tracking.System.Application.Bug.Tracking.Application.repository;
-
-
-import Bug.Tracking.System.Application.Bug.Tracking.Application.entity.Sprint;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SprintRepository extends JpaRepository<Sprint, Long> {
-}
-
-
->>>>>>> 7597e7e0eba66a899deb947e73815869450259fd
