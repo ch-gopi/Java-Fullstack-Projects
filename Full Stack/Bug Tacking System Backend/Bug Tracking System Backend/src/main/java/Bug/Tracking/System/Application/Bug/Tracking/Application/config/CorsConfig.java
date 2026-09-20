8da04eeef6ad302dacac8ed6baf5ca99f03bc877
GOPI CH <ch.gopi7893@gmail.com>
2026-09-20 10:51:14 +0530

    Remove duplicate CorsConfig class definition

--- Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/config/CorsConfig.java
+++ Full Stack/Bug Tacking System Backend/Bug Tracking System Backend/src/main/java/Bug/Tracking/System/Application/Bug/Tracking/Application/config/CorsConfig.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Bug.Tracking.System.Application.Bug.Tracking.Application.config;
 
 import org.springframework.context.annotation.Bean;
@@ -23,29 +22,3 @@ public class CorsConfig {
         };
     }
 }
-=======
-package Bug.Tracking.System.Application.Bug.Tracking.Application.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/uploads/images/**")
-                        .allowedOrigins("/api/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET");
-            }
-        };
-    }
-}
->>>>>>> 7597e7e0eba66a899deb947e73815869450259fd
